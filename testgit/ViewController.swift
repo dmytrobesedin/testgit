fac6f42a62a4685717bb8e868fe636fd62562c95
dmytrobesedin <dbesedin7@gmail.com>
2017-11-30 15:32:35 +0200

    new file

---
+++ testgit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Дмитрий Беседин on 30.11.2017.
+//  Copyright © 2017 dmytrobesedin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
